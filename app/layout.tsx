417acbb746e943bec699283e50d9ad10a3bab799
supunyasantha <supunyasantha.official@gmail.com>
2026-03-07 13:59:33 +0530

    remove anime character componenet.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+
 import "./globals.css";
 import CustomCursor from "@/components/Cursor";
 
@@ -10,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <CustomCursor /> 
+      
         {children}
       </body>
     </html>
