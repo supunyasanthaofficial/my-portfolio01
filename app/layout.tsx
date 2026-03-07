74f5f1c80d094c8e72ccaa1a8de183ac7c7dc385
supunyasantha <supunyasantha.official@gmail.com>
2026-03-07 13:25:20 +0530

    Updated layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <CustomCursor />
+      <body>
+        <CustomCursor /> 
+        {children}
+      </body>
     </html>
   );
 }
