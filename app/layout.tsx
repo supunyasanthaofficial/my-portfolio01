1c1782d5b87c3a4d496a89bd1ddcb023f5268372
supun_yasantha034 <supunyasantha.official@gmail.com>
2026-08-26 12:12:53 +0530

    Add metadata for SEO optimization in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from "next";
+import type React from "react";
 import { ThemeProvider } from "@/provider/ThemeProvider";
-import "./globals.css";
 import CustomCursor from "@/components/Cursor";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Supun Yasantha | Portfolio",
+  description: "Full-stack Developer & Editor Portfolio",
+};
 
 export default function RootLayout({
   children,
@@ -12,10 +19,10 @@ export default function RootLayout({
       <body>
         <ThemeProvider>
           <CustomCursor />
-
           {children}
         </ThemeProvider>
       </body>
     </html>
   );
 }
+
