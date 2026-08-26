54f5a8732a262e111408804240e05cf82b651032
supun_yasantha034 <supunyasantha.official@gmail.com>
2026-08-26 12:12:37 +0530

    Add turbopack configuration to next.config.ts for improved build performance

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+import path from "path";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
