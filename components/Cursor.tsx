f39088a832da69a4ffcaddfed23f82ae4ccb6e18
supun_yasantha034 <supunyasantha.official@gmail.com>
2026-08-26 12:13:06 +0530

    Remove unused state for mouse position in CustomCursor component

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { motion, useSpring } from "framer-motion";
 function CustomCursor() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
   const springConfig = { damping: 25, stiffness: 200 };
   const cursorX = useSpring(0, springConfig);
   const cursorY = useSpring(0, springConfig);
